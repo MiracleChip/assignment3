7427dfc6d616f2cda05bffdcfb03c8ad00af74f5
MiracleChip <135042825+MiracleChip@users.noreply.github.com>
2024-11-01 13:41:41 +0200

    Update ctrl_unit.sv

--- ctrl_unit.sv
+++ ctrl_unit.sv
@@ -1,26 +1,29 @@
-`include "defines.sv"
-module ctrl_unit 
+module ctrl_unit # (
+    parameter int NUM_FLOORS = 16,               // Number of floors
+    parameter int FLOOR_BITS = $clog2(NUM_FLOORS) // bits to represent the number of floors
+)    
     (
 
-    input logic [`FLOOR_BITS-1:0] req,
+    input logic [FLOOR_BITS-1:0] req,
     input logic clk,
     input logic resetN,
     output logic up, 
     output logic down, 
     output logic open, //open 1 if not open 0
     //output logic closed,
-    output logic [`FLOOR_BITS-1:0] floor // ssd output -- the reached floor 
+    output logic [FLOOR_BITS-1:0] floor // ssd output -- the reached floor 
 );
 
 //add instatiation of request_resolver (omar's part)
 
 
 //timeunit 1ns/1ns;
+localparam int NUM_FLOOR=10;
 localparam int COUNTER_FINAL=50;
 
 logic [31:0] counter;
 logic [1:0] timer;
-logic en;
+logic en, open_move, flag;
 
 //define states -- 
 typedef enum logic [1:0]{
@@ -32,12 +35,13 @@ typedef enum logic [1:0]{
 
 state current_state, next_state;
 
-logic [`FLOOR_BITS-1:0] current_floor, target_floor, previous;
+logic [3:0] current_floor, target_floor;
 //logic req_ack;
 
 //will extract from req
 //assign opcode =req[9:8];
-assign target_floor = req[`FLOOR_BITS-1:0];
+assign target_floor = req[3:0];
+
 //logic flag;
 
 /// Current State Logic -- sequential logic
@@ -49,6 +53,13 @@ always_ff @(posedge clk or negedge resetN) begin
     end
     else begin
         current_state <= next_state;
+        if(current_state == close_state && current_floor == target_floor) begin
+            //req_ack <=1;
+        end
+       // else if (next_state == open_state) begin
+            
+       // end
+        //flag<=0;
     end
 end
 
@@ -66,21 +77,18 @@ always @(posedge clk or negedge resetN) begin
             //     current_floor<=target_floor;
             // end
 
-            if(current_state==up_state && current_floor < target_floor && timer ==1 && counter == COUNTER_FINAL-1) begin
+            if(current_state==up_state && current_floor < target_floor && timer == 1 && counter == COUNTER_FINAL - 1) begin
                 current_floor <=current_floor+1;
             end
-            else if (current_state == down_state && current_floor > target_floor && timer ==1 && counter == COUNTER_FINAL-1) begin
+            else if (current_state == down_state && current_floor > target_floor && timer == 1 && counter == COUNTER_FINAL - 1) begin
                 current_floor <= current_floor-1;
             end
-            else if(current_state==open_state && current_floor == target_floor && timer ==1 && counter == COUNTER_FINAL-1) begin
+            else if(current_state==open_state && current_floor == target_floor && timer == 1 && counter == COUNTER_FINAL - 1) begin
                 //flag<=0;
                 current_floor<=target_floor;
                 //req_ack<=1;
                 //flag<=1;
                 //d_stopped<=current_floor;
-            end
-            else begin
-                //flag
             end
     end
 end
@@ -96,31 +104,43 @@ always_comb begin
         close_state: begin // close
             if ((current_floor)  < target_floor) begin
                 next_state = up_state;
+                open_move=0;
+                flag=0;
                 //req_ack=1;
                 //val=1;
             end 
             else if ((current_floor)  > target_floor) begin
                 next_state = down_state;
+                open_move=0;
+                flag=0;
                 //req_ack=1;
+            end 
+            else if ((current_floor == floor && flag==1)) begin
+                next_state = open_state;
+                flag=0;
             end 
         end
 
         open_state: begin // open 
-        if(timer==1) begin
+        if(timer == 1 && counter == COUNTER_FINAL - 1) begin
 
-                if((current_floor)  == target_floor) begin
+               // if((current_floor)  == target_floor) begin
                     next_state=close_state;
+                    open_move=0;
+                    //flag=0;
                     //req_ack =0;
                     //val=req_ack;
                 //req_ack=1; //it has closed
-            end
+            //send
         end
         end
 
         up_state: begin
-            //if(timer==2) begin
+            //if(timer == 1 && counter == COUNTER_FINAL - 1) begin
                 if ((current_floor) == target_floor) begin
                     next_state = open_state;
+                    open_move=1;
+                    flag=1;
                     //req_ack =1;
                 end
         end
@@ -129,12 +149,16 @@ always_comb begin
            //if(timer==2) begin
                 if ((current_floor)  == target_floor) begin
                     next_state = open_state;
+                    open_move=1;
+                    flag=1;
                     //req_ack =1;
                 end
         end
 
         default:  begin
             next_state = close_state;
+            open_move=0;
+            flag=0;
             //req_ack=0;
         end
     endcase
@@ -174,24 +198,30 @@ always_ff @(posedge clk or negedge resetN) begin
         timer<=0;
     end
     else begin
-        if(((current_state==up_state &&  next_state == up_state )|| (current_state==down_state && current_state==next_state)|| current_state==open_state))begin
-        end
-            if (counter<COUNTER_FINAL-1)begin
+        if(((current_state==up_state &&  next_state == up_state )|| (current_state==down_state && current_state==next_state)|| current_state==open_state)) begin
+        
+            if (counter<COUNTER_FINAL - 1)begin
                 counter<=counter+1;
                 en<=0;
             end
-            else if (counter == COUNTER_FINAL-1) begin
+            else if (counter == COUNTER_FINAL - 1) begin
                 en<=1;
-                timer<=timer+1;
                 counter<=0;
+				timer<=timer+1;
                 // timer=timer+1;
                 // count=0;
             end
-            if (timer ==1 && counter == COUNTER_FINAL-1) begin
+            //if(en==1) begin
+                //timer<=timer+1;
+                //count<=0;
+              //  en<=0;
+            //end
+            if (timer == 1 && counter == COUNTER_FINAL - 1) begin
                 timer<=0;
                 //en=0;
                 //count=0;
             end
+		end	
     end
 end
 
